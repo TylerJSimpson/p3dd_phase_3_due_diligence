0ab13fc13c9c07865ed7be31aad2877251ab9d77
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-14 18:07:22 -0400

    Create aact_postgres_to_gcs.py

---
+++ project/prefect_pipelines/aact_postgres_to_gcs.py
@@ -0,0 +1,72 @@
+import psycopg2
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from google.cloud import storage
+from configparser import ConfigParser
+from prefect import task, Flow
+
+@task
+def read_postgresql_credentials(config_file_path):
+    config = ConfigParser()
+    config.read(config_file_path)
+
+    return {
+        'host': config['postgresql']['host'],
+        'port': config['postgresql']['port'],
+        'database': config['postgresql']['database'],
+        'username': config['postgresql']['username'],
+        'password': config['postgresql']['password']
+    }
+
+@task
+def read_gcs_credentials(config_file_path):
+    config = ConfigParser()
+    config.read(config_file_path)
+
+    return {
+        'bucket': config['gcs']['bucket'],
+        'credentials_path': config['gcs']['credentials_path']
+    }
+
+@task
+def query_postgresql(pg_credentials, query):
+    conn = psycopg2.connect(
+        host=pg_credentials['host'],
+        port=pg_credentials['port'],
+        database=pg_credentials['database'],
+        user=pg_credentials['username'],
+        password=pg_credentials['password']
+    )
+
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    return df
+
+@task
+def write_parquet_file(df, file_path):
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, file_path)
+
+@task
+def upload_to_gcs(file_path, gcs_credentials_path, bucket_name, gcs_object_name):
+    client = storage.Client.from_service_account_json(gcs_credentials_path)
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(gcs_object_name)
+    blob.upload_from_filename(file_path)
+
+# Define the flow for the pipeline
+with Flow("postgresql-to-gcs") as flow:
+    pg_creds = read_postgresql_credentials('/home/tjsimpson/project/configuration/config.ini')
+    gcs_creds = read_gcs_credentials('/home/tjsimpson/project/configuration/config.ini')
+
+    data = query_postgresql(pg_creds, "SELECT * FROM studies")
+
+    parquet_file = 'data.parquet'
+    write_parquet_file(data, parquet_file)
+
+    upload_to_gcs(parquet_file, gcs_creds['credentials_path'], gcs_creds['bucket'], 'data.parquet')
+
+# Run the pipeline
+flow.run()
