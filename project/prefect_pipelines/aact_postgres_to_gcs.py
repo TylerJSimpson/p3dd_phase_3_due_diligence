bef2cd79ff29e26569e306df2503e4e91b0286bd
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-03-15 18:41:59 -0400

    Update aact_postgres_to_gcs.py
    first iteration of working pipeline

--- project/prefect_pipelines/aact_postgres_to_gcs.py
+++ project/prefect_pipelines/aact_postgres_to_gcs.py
@@ -1,28 +1,21 @@
 import psycopg2
-import logging
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 from google.cloud import storage
 from configparser import ConfigParser
 from prefect import task, Flow
+from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
 
-# Initialize the logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-# Configure the logger to output to a file in GCS
-storage_client = storage.Client.from_service_account_json('/path/to/credentials.json')
-bucket = storage_client.bucket('my-bucket')
-log_file = bucket.blob('logs/pipeline.log')
-handler = logging.FileHandler(log_file)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+"""
+TO-DO:
+add logging
+parameterize? at least file name
+"""
 
 @task
 def read_postgresql_credentials(config_file_path):
-    logger.info('Reading PostgreSQL credentials from config file')
     config = ConfigParser()
     config.read(config_file_path)
 
@@ -35,19 +28,7 @@ def read_postgresql_credentials(config_file_path):
     }
 
 @task
-def read_gcs_credentials(config_file_path):
-    logger.info('Reading GCS credentials from config file')
-    config = ConfigParser()
-    config.read(config_file_path)
-
-    return {
-        'bucket': config['gcs']['bucket'],
-        'credentials_path': config['gcs']['credentials_path']
-    }
-
-@task
 def query_postgresql(pg_credentials, query):
-    logger.info('Connecting to PostgreSQL database')
     conn = psycopg2.connect(
         host=pg_credentials['host'],
         port=pg_credentials['port'],
@@ -63,29 +44,26 @@ def query_postgresql(pg_credentials, query):
 
 @task
 def write_parquet_file(df, file_path):
-    logger.info('Writing data to Parquet file')
     table = pa.Table.from_pandas(df)
     pq.write_table(table, file_path)
 
+
 @task
-def upload_to_gcs(file_path, gcs_credentials_path, bucket_name, gcs_object_name):
-    logger.info('Uploading Parquet file to GCS')
-    client = storage.Client.from_service_account_json(gcs_credentials_path)
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(gcs_object_name)
-    blob.upload_from_filename(file_path)
+def upload_to_gcs(file_path, bucket_name, destination_blob_name):
+    gcs_bucket = GcsBucket.load(bucket_name)
+    gcs_bucket.upload_from_path(from_path=file_path, to_path=destination_blob_name)
 
-# Define the flow for the pipeline
-with Flow("postgresql-to-gcs") as flow:
+
+@Flow
+def postgresql_to_gcs():
     pg_creds = read_postgresql_credentials('/home/tjsimpson/project/configuration/config.ini')
-    gcs_creds = read_gcs_credentials('/home/tjsimpson/project/configuration/config.ini')
 
-    data = query_postgresql(pg_creds, "SELECT * FROM studies")
+    data = query_postgresql(pg_creds, "SELECT * FROM studies LIMIT 10")
 
-    parquet_file = 'data.parquet'
+    parquet_file = 'aact_studies_test_03152023_01.parquet'
     write_parquet_file(data, parquet_file)
 
-    upload_to_gcs(parquet_file, gcs_creds['credentials_path'], gcs_creds['bucket'], 'data.parquet')
+    upload_to_gcs(parquet_file, 'zoom-gcs', 'project/bronze/aact_studies_test_03152023_01.parquet')
 
-# Run the pipeline
-flow.run()
+if __name__ == "__main__":
+    postgresql_to_gcs()
