b1f58b68558b4004e36ed97423d8dc935642f39b
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-04-02 15:57:47 -0400

    Update linkedin_jobs.sql

--- project/bigquery_sql/linkedin_jobs.sql
+++ project/bigquery_sql/linkedin_jobs.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE EXTERNAL TABLE `dtc-de-0315.bronze.linkedin_jobs_external`
 OPTIONS (
   format = 'parquet',
-  uris = ['gs://dtc_data_lake_dtc-de-0315/project/bronze/linkedin_jobs/linkedin_jobs_04022023_194438.parquet'] --note this can change just using this as an example to get schema
+  uris = ['gs://dtc_data_lake_dtc-de-0315/project/bronze/linkedin_jobs/linkedin_jobs_04022023_194438.parquet']
 );
 
 CREATE OR REPLACE TABLE `dtc-de-0315.bronze.linkedin_jobs`
 AS
 SELECT  *
-FROM    `dtc-de-0315.bronze.aact_studies_external`
+FROM    `dtc-de-0315.bronze.linkedin_jobs_external`
 ; 
