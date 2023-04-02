4a14fddd6c6ca89f837974008cb7bfd35bc479d6
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2023-04-02 15:56:29 -0400

    Update aact_postgres_to_gcs_and_bq.py
    Significantly updated. Added the bigquery portion, modified the file path in GCS, added comments.

--- project/prefect_pipelines/aact_postgres_to_gcs_and_bq.py
+++ project/prefect_pipelines/aact_postgres_to_gcs_and_bq.py
@@ -1,7 +1,12 @@
 """
+aact_postgres_to_gcs_and_bq
 
+(1) Queries AACT Postgres database
+(2) Creates Parquet file
+(3) Writes Parquet file to GCS and BigQuery
 """
 
+#Import packages
 import psycopg2
 import pandas as pd
 import pyarrow as pa
@@ -14,13 +19,12 @@ from prefect_gcp.cloud_storage import GcsBucket
 import datetime
 import os
 
-"""
-TO-DO:
-add logging
-"""
 
 @task
 def read_postgresql_credentials(config_file_path):
+    """
+    Reads postgresql credentials from local filepath on vm and assigns them to variables
+    """
     config = ConfigParser()
     config.read(config_file_path)
 
@@ -32,8 +36,12 @@ def read_postgresql_credentials(config_file_path):
         'password': config['postgresql']['password']
     }
 
+
 @task
 def query_postgresql(pg_credentials, query):
+    """
+    Queries postgresql AACT database main fact table [studies] and writes it to pandas dataframe
+    """
     conn = psycopg2.connect(
         host=pg_credentials['host'],
         port=pg_credentials['port'],
@@ -47,25 +55,39 @@ def query_postgresql(pg_credentials, query):
 
     return df
 
+
 @task
 def write_parquet_file(df, file_path):
+    """
+    Write pandas dataframe to parquet file
+    """
     table = pa.Table.from_pandas(df)
     pq.write_table(table, file_path)
 
 
 @task
 def upload_to_gcs(file_path, bucket_name, destination_blob_name):
+    """
+    Uploads parquet file to GCS
+    """
     gcs_bucket = GcsBucket.load(bucket_name)
     gcs_bucket.upload_from_path(from_path=file_path, to_path=destination_blob_name)
 
 
 @task
 def delete_local_file(file_path):
+    """
+    Deletes local file on vm due to GCS being used as the primary storage
+    """
     os.remove(file_path)
+
 
 @task()
 def write_bq(data: pd.DataFrame) -> None:
-    """Write DataFrame to BiqQuery"""
+    """
+    Write pandas dataframe to BiqQuery table bronze.aact_studies
+    Replaces (truncate + write) on each run
+    """
 
     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
 
@@ -110,21 +132,28 @@ def write_bq(data: pd.DataFrame) -> None:
     job.result()
 '''
 
+
 @Flow
 def aact_postgres_to_gcs_and_bq():
+ 
+    #Get Postgresql credentials from local config file
     pg_creds = read_postgresql_credentials('/home/tjsimpson/project/configuration/config.ini')
 
-    data = query_postgresql(pg_creds, "SELECT * FROM studies")
+    #This query selects the entire [studies] table and adds the key column linked_jobs_key by combining 'nct_id'_'source' and removing all spaces and special characters
+    data = query_postgresql(pg_creds, "SELECT   *, REGEXP_REPLACE(CONCAT(nct_id, '_', source), '[^a-zA-Z0-9_]', '', 'g') AS linkedin_jobs_key FROM studies")
 
+    #Building parquet file
     current_datetime = datetime.datetime.now().strftime('%m%d%Y_%H%M%S')
     parquet_file = f'aact_studies_{current_datetime}.parquet'
     write_parquet_file(data, parquet_file)
 
-    upload_to_gcs(parquet_file, 'zoom-gcs', f'project/bronze/{parquet_file}')
+    #Upload parquet to GCS
+    upload_to_gcs(parquet_file, 'zoom-gcs', f'project/bronze/aact_studies/{parquet_file}')
 
+    #Delete local file
     delete_local_file(parquet_file)
 
-    # Write Parquet file to BigQuery
+    #Write data to BigQuery
     write_bq(data)
 
 if __name__ == "__main__":
