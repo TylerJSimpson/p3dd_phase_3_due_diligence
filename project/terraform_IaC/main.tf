bd4193f1836b552c1023473c4f53dafb3d1dc1a1
Tyler Simpson <Tjsimpson@mix.wvu.edu>
2023-04-26 21:05:39 -0400

    Feature: Terraform IaC backbone working
    Worked out the requirements and built out terraform for the project
    
    -added python terraform to venv requirements.txt
    -got rid of old_main.tf
    -updated main.tf, variables.tf, and terraform.tfvars
    -tfvars is placeholder no data exposure
    -working build and destroy, some issue with API destroy
    -TODO: build out the tables
    -TODO: add vnet/subnet

--- project/terraform_IaC/main.tf
+++ project/terraform_IaC/main.tf
@@ -1,10 +1,9 @@
 provider "google" {
-  version = "3.74.0"
-  region  = var.region
+  region = var.region
 }
 
 resource "google_project" "project" {
-  name            = "prj-p3dd-data-${var.environment}-${var.increment}"  
+  name            = "prj-p3dd-data-${var.environment}-${var.increment}"
   project_id      = "prj-p3dd-data-${var.environment}-${var.increment}"
   billing_account = var.billing_account
 }
@@ -17,7 +16,7 @@ resource "google_service_account" "service_account" {
 
 resource "google_project_iam_binding" "iam_binding" {
   project = google_project.project.project_id
-  role    = "roles/owner"
+  role    = "roles/editor"
 
   members = [
     "serviceAccount:${google_service_account.service_account.email}",
@@ -57,8 +56,8 @@ resource "google_storage_bucket" "gcs_bucket" {
       age = 30
     }
     action {
-      type             = "SetStorageClass"
-      storage_class    = "NEARLINE"
+      type          = "SetStorageClass"
+      storage_class = "NEARLINE"
     }
   }
 
@@ -67,8 +66,8 @@ resource "google_storage_bucket" "gcs_bucket" {
       age = 90
     }
     action {
-      type             = "SetStorageClass"
-      storage_class    = "COLDLINE"
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
     }
   }
 
@@ -77,78 +76,30 @@ resource "google_storage_bucket" "gcs_bucket" {
       age = 365
     }
     action {
-      type             = "SetStorageClass"
-      storage_class    = "ARCHIVE"
+      type          = "SetStorageClass"
+      storage_class = "ARCHIVE"
     }
   }
 }
 
+resource "google_project_service" "bigquery" {
+  service                    = "bigquery.googleapis.com"
+  project                    = google_project.project.project_id
+  disable_dependent_services = true
+}
 
 resource "google_bigquery_dataset" "bigquery_dataset_bronze" {
-  dataset_id   = "bq-bronze-p3dd-data-${var.environment}-${var.increment}"
-  location     = var.region
-  project      = google_project.project.project_id
-  description  = "p3dd BigQuery Bronze Schema ${var.environment}-${var.increment}"
+  dataset_id                  = "bronze"
+  location                    = var.region
+  project                     = google_project.project.project_id
+  description                 = "p3dd BigQuery Bronze Schema ${var.environment}-${var.increment}"
   default_table_expiration_ms = "3600000"
 }
 
 resource "google_bigquery_table" "default" {
-  dataset_id = "${google_bigquery_dataset.bigquery_dataset_bronze.dataset_id}"
-  table_id   = "nct"
-
-  labels = {
-    env = "${var.environment}"
-  }
-
-  schema = <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
-
-}
-
-resource "google_bigquery_table" "default" {
-  dataset_id = "${google_bigquery_dataset.bigquery_dataset_bronze.dataset_id}"
-  table_id   = "figi"
-
-  labels = {
-    env = "${var.environment}"
-  }
-
-  schema = <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
-
-}
-
-resource "google_bigquery_table" "default" {
-  dataset_id = "${google_bigquery_dataset.bigquery_dataset_bronze.dataset_id}"
+  dataset_id = google_bigquery_dataset.bigquery_dataset_bronze.dataset_id
   table_id   = "jobs"
+  project    = google_project.project.project_id
 
   labels = {
     env = "${var.environment}"
@@ -157,17 +108,29 @@ resource "google_bigquery_table" "default" {
   schema = <<EOF
 [
   {
-    "name": "permalink",
-    "type": "STRING",
+    "name": "figi_primary_key",
+    "type": "INTEGER",
     "mode": "NULLABLE",
-    "description": "The Permalink"
+    "description": "Placeholder"
   },
   {
-    "name": "state",
+    "name": "linkedin_source_name",
     "type": "STRING",
     "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
+    "description": "Placeholder"
+  },
+  {
+    "name": "num_jobs",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+    "description": "Placeholder"
+  },
+  {
+    "name": "timestamp",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE",
+    "description": "Placeholder"
+  }    
 ]
 EOF
 
--- project/terraform_IaC/variables.tf
+++ project/terraform_IaC/variables.tf
@@ -8,11 +8,6 @@ variable "increment" {
   description = "The increment of the GCP project ie '001', '002', '003'"
 }
 
-variable "project_id" {
-  type        = string
-  description = "The ID of the GCP project to create"
-}
-
 variable "billing_account" {
   type        = string
   description = "The ID of the billing account to associate with the new project"
@@ -22,19 +17,4 @@ variable "region" {
   type        = string
   default     = "us-east4"
   description = "The region to create resources in"
-}
-
-variable "gcs_bucket_name" {
-  type        = string
-  description = "The name of the GCS bucket to create"
-}
-
-variable "bigquery_dataset_name" {
-  type        = string
-  description = "The name of the BigQuery dataset to create"
-}
-
-variable "service_account_name" {
-  type        = string
-  description = "The name of the service account to create"
 }
