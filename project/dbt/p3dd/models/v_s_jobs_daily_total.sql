4eb39dc043259ffde6b32467a7b7c2496e48a6ce
Tyler Simpson <Tjsimpson@mix.wvu.edu>
2023-05-08 20:39:28 -0400

    Feature: dbt added
    Implemented dbt for promotion up from bronze schema
    
    -dbt implemented successfully
    -created first materialized view v_s_jobs_daily_total.sql
    -created schema.yml
    -created placeholder.sql which has multiple views to be added later

---
+++ project/dbt/p3dd/models/v_s_jobs_daily_total.sql
@@ -0,0 +1,19 @@
+{{ config(
+    materialized='view',
+    schema='silver',
+    alias='jobs_daily_total'
+) }}
+
+SELECT
+        DATE(DATE_TRUNC(timestamp, DAY)) AS day,
+        SUM(num_jobs) AS total_num_jobs
+FROM    `dtc-de-0315.bronze.jobs`
+WHERE   figi_primary_key NOT IN (
+            SELECT figi_primary_key
+            FROM `dtc-de-0315.bronze.jobs`
+            WHERE num_jobs >= 1000
+            GROUP BY figi_primary_key
+        )
+GROUP   BY day
+HAVING  MAX(num_jobs) < 1000                            --currently accuracy drops above num_jobs of 1000 so these need to be excluded
+;
