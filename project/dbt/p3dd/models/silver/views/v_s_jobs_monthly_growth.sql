768aefb8aeb9f3b326d026e5c08e4a1fab09a878
Tyler Simpson <Tjsimpson@mix.wvu.edu>
2023-05-12 19:42:59 -0400

    Feat: dbt
    Updated the dbt package, still not running
    
    -added monthly job growth view
    -removed placeholder as loss views are not needed now
    -need to look at adding HAVING MAX(num_jobs) < 1000 in all views

--- project/dbt/p3dd/models/silver/views/v_s_jobs_monthly_growth.sql
+++ project/dbt/p3dd/models/silver/views/v_s_jobs_monthly_growth.sql
@@ -11,14 +11,15 @@ SELECT  figi.figi_source_name,
         AVG(prev.num_jobs) AS prev_week_avg_num_jobs,
         curr.num_jobs AS curr_day_num_jobs,
         CASE 
-        WHEN AVG(prev.num_jobs) = 0 THEN NULL
-        ELSE (curr.num_jobs - AVG(prev.num_jobs)) / AVG(prev.num_jobs)
+            WHEN AVG(prev.num_jobs) = 0 THEN NULL
+            ELSE (curr.num_jobs - AVG(prev.num_jobs)) / AVG(prev.num_jobs)
         END AS growth_pct
 FROM    `dtc-de-0315.bronze.jobs` curr
         JOIN `dtc-de-0315.bronze.jobs` prev
-        ON curr.figi_primary_key = prev.figi_primary_key
-        AND DATE(curr.timestamp) = DATE_SUB(DATE(prev.timestamp), INTERVAL 30 DAY)
+            ON curr.figi_primary_key = prev.figi_primary_key
+            AND DATE(curr.timestamp) = DATE_SUB(DATE(prev.timestamp), INTERVAL 30 DAY)
         JOIN `dtc-de-0315.bronze.figi` figi
-        ON curr.figi_primary_key = figi.figi_primary_key
+            ON curr.figi_primary_key = figi.figi_primary_key
 GROUP   BY figi.figi_source_name, figi.ticker, curr.num_jobs
 ORDER   BY growth_pct DESC
+;
